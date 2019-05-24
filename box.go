8ec9cc5ccc978e6033e744e81bb3154ce37ae0ff
Torben Schinke <torben.schinke@worldiety.de>
2019-05-24 14:21:52 +0200

    Box returns itself for setters to have a fluid builder like declaration in gomobile targets

--- box.go
+++ box.go
@@ -31,73 +31,87 @@ func (i *Box) TypeName() string {
 }
 
 // Set unboxes the value from the other box and sets it into this box
-func (i *Box) Set(other *Box) {
+func (i *Box) Set(other *Box) *Box {
 	i.Value = other.Unbox()
+	return i
 }
 
-// SetString puts the value into the box
-func (i *Box) SetString(v string) {
+// SetString puts the value into the box and returns the box.
+func (i *Box) SetString(v string) *Box {
 	i.Value = v
+	return i
 }
 
-// SetFloat64 puts the value into the box
-func (i *Box) SetFloat64(v float64) {
+// SetFloat64 puts the value into the box and returns the box.
+func (i *Box) SetFloat64(v float64) *Box {
 	i.Value = v
+	return i
 }
 
-// SetFloat32 puts the value into the box
-func (i *Box) SetFloat32(v float32) {
+// SetFloat32 puts the value into the box and returns the box.
+func (i *Box) SetFloat32(v float32) *Box {
 	i.Value = v
+	return i
 }
 
-// SetInt64 puts the value into the box
-func (i *Box) SetInt64(v int64) {
+// SetInt64 puts the value into the box and returns the box.
+func (i *Box) SetInt64(v int64) *Box {
 	i.Value = v
+	return i
 }
 
-// SetInt32 puts the value into the box
-func (i *Box) SetInt32(v int32) {
+// SetInt32 puts the value into the box and returns the box.
+func (i *Box) SetInt32(v int32) *Box {
 	i.Value = v
+	return i
 }
 
-// SetInt16 puts the value into the box
-func (i *Box) SetInt16(v int16) {
+// SetInt16 puts the value into the box and returns the box.
+func (i *Box) SetInt16(v int16) *Box {
 	i.Value = v
+	return i
 }
 
-// SetInt8 puts the value into the box
-func (i *Box) SetInt8(v int8) {
+// SetInt8 puts the value into the box and returns the box.
+func (i *Box) SetInt8(v int8) *Box {
 	i.Value = v
+	return i
 }
 
-// SetInt puts the value into the box
-func (i *Box) SetInt(v int) {
+// SetInt puts the value into the box and returns the box.
+func (i *Box) SetInt(v int) *Box {
 	i.Value = v
+	return i
 }
 
-// SetBool puts the value into the box
-func (i *Box) SetBool(v bool) {
+// SetBool puts the value into the box and returns the box.
+func (i *Box) SetBool(v bool) *Box {
 	i.Value = v
+	return i
 }
 
-// SetSlice unwraps the slice from the box and boxes it
-func (i *Box) SetSlice(v *Slice) {
+// SetSlice unwraps the slice from the box and boxes it and returns the box.
+func (i *Box) SetSlice(v *Slice) *Box {
 	i.Value = v.Slice
+	return i
 }
 
-// SetStrSlice unwraps the slice from the box and boxes it
-func (i *Box) SetStrSlice(v *StrSlice) {
+// SetStrSlice unwraps the slice from the box and boxes it and returns the box.
+func (i *Box) SetStrSlice(v *StrSlice) *Box {
 	i.Value = v.Slice
+	return i
 }
 
-// SetStrMap takes the map and unwraps the map into this box.
-func (i *Box) SetStrMap(v *StrMap) {
+// SetStrMap takes the map and unwraps the map into this box and returns the box.
+func (i *Box) SetStrMap(v *StrMap) *Box {
 	i.Value = v.Map
+	return i
 }
 
-// SetMap takes the map and unwraps the map into this box.
-func (i *Box) SetMap(v *Map) {
+// SetMap takes the map and unwraps the map into this box and returns the box.
+func (i *Box) SetMap(v *Map) *Box {
 	i.Value = v.Map
+	return i
 }
 
 // IsNil checks if the box or the boxed interface is nil. Usually you don't need that.
